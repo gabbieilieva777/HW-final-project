977f4b98e05297cd3eb64c4bca68e70771bd8085
gabrielailieva <gabbieilieva777@gmail.com>
2026-06-15 16:11:44 +0200

    create live detection file and set up paths and imports

---
+++ live_detection.py
@@ -0,0 +1,30 @@
+# Program to recognise sleep gestures in real time
+# Then play a sound effect and send data to lights and other visual components of
+# the installation
+
+
+# IMPORTS
+from __future__ import annotations
+
+import os
+import warnings
+from pathlib import Path
+import cv2
+import joblib
+import mediapipe as mp
+import numpy as np
+import pandas as pd
+
+# warning cleanup
+warnings.filterwarnings(
+    "ignore",
+    message=r".*SymbolDatabase\.GetPrototype\(\) is deprecated.*",
+)
+warnings.filterwarnings(
+    "ignore",
+    message=r".*X does not have valid feature names.*",
+)
+
+# paths
+MODEL = Path("model/sleep_gesture_svm.joblib")
+AUDIO_PATH = Path("placeholder.wav")   # sound to play when sleep mode is initiated
